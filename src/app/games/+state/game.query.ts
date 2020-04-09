94631987e53bb503e365606e519eea8a14286a5a
Jackseed <vinkho.c@gmail.com>
2020-04-09 22:54:20 +0200

    current game list

--- src/app/games/+state/game.query.ts
+++ src/app/games/+state/game.query.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
 import { GameStore, GameState } from './game.store';
+import { Game } from './game.model';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GameQuery extends QueryEntity<GameState> {
 
-  constructor(protected store: GameStore) {
+  constructor(
+    protected store: GameStore,
+    private afAuth: AngularFireAuth,
+  ) {
     super(store);
+  }
+
+  get playerGames(): Observable<Game[]> {
+    const user = this.afAuth.auth.currentUser;
+    return this.selectAll({
+      filterBy: game => game.playerIds.includes(user.uid)
+    });
+  }
+
+  get otherGames(): Observable<Game[]> {
+    const user = this.afAuth.auth.currentUser;
+    return this.selectAll({
+      filterBy: game => !game.playerIds.includes(user.uid)
+    });
   }
 
 }
