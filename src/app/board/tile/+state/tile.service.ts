022c1950c5ae64ffa66409cfd16c1144ca621c76
Jackseed <vinkho.c@gmail.com>
2020-04-20 09:11:48 +0200

    set placement tiles

--- src/app/board/tile/+state/tile.service.ts
+++ src/app/board/tile/+state/tile.service.ts
@@ -3,7 +3,8 @@ import { Unit, UnitStore, UnitService } from '../../unit/+state';
 import { TileQuery } from './tile.query';
 import { TileStore } from './tile.store';
 import { Tile, createTile } from './tile.model';
-import { boardCols, boardMaxTiles } from 'src/app/games/+state';
+import { boardCols, boardMaxTiles, unitPlacementMargin, GameQuery } from 'src/app/games/+state';
+import { PlayerQuery } from '../../player/+state';
 
 @Injectable({ providedIn: 'root' })
 export class TileService {
@@ -11,12 +12,16 @@ export class TileService {
   constructor(
     private store: TileStore,
     private query: TileQuery,
+    private gameQuery: GameQuery,
+    private playerQuery: PlayerQuery,
     private unitStore: UnitStore,
     private unitService: UnitService,
   ) {}
 
   setTiles() {
     const tiles: Tile[] = [];
+    const gameStatus = this.gameQuery.getActive().status;
+
     for (let i = 0; i < boardCols; i++) {
       for (let j = 0; j < boardCols; j++) {
         const tileId = j + boardCols * i;
@@ -26,7 +31,43 @@ export class TileService {
         }
       }
     }
+
     this.store.set(tiles);
+
+    if (gameStatus === 'placement') {
+      this.setPlacementTiles();
+    }
+  }
+
+  // TODO : factorize with unit service defaultPositionUnits()
+  setPlacementTiles() {
+    const player = this.playerQuery.getActive();
+    const placementTilesArea = (unitPlacementMargin + 1) * ( boardCols - (unitPlacementMargin * 2));
+    const placementTiles: number[] = [];
+
+    let y = unitPlacementMargin;
+    let x = unitPlacementMargin;
+    let j = 1;
+    if (player.color === 'black') {
+      x = boardCols - (unitPlacementMargin + 1);
+    }
+
+    for (let i = 0; i < placementTilesArea; i++) {
+      const tileId = x + y * boardCols;
+      placementTiles.push(tileId);
+      if (y < (boardCols - (unitPlacementMargin + 1))) {
+        y++;
+      } else {
+        if (player.color === 'black') {
+          x = boardCols - (unitPlacementMargin + 1) + j;
+        } else {
+          x = unitPlacementMargin - j;
+        }
+        y = unitPlacementMargin;
+        j++;
+      }
+    }
+    this.markAsReachable(placementTiles);
   }
 
   markAsSelected(tileId: number, unit: Unit) {
@@ -39,7 +80,11 @@ export class TileService {
     let reachableTileIds = this.query.getAdjacentTiles(unit.tileId, unit.move);
     // remove the tile id of the unit
     reachableTileIds = reachableTileIds.filter(tileId => tileId !== unit.tileId);
-    this.store.update(reachableTileIds, ({ isReachable: true }));
+    this.markAsReachable(reachableTileIds);
+  }
+
+  markAsReachable(tileIds: number[]) {
+    this.store.update(tileIds, ({ isReachable: true }));
   }
 
   public moveSelectedUnit(unit: Unit, tileId: number) {
