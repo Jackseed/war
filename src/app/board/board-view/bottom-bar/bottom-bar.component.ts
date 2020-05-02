ebdf9873c1c8edac54433a047eef3f0d8636b646
Jackseed <vinkho.c@gmail.com>
2020-05-02 09:22:27 +0200

    skip button

--- src/app/board/board-view/bottom-bar/bottom-bar.component.ts
+++ src/app/board/board-view/bottom-bar/bottom-bar.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
-import { UnitService } from '../../unit/+state';
-import { GameService, GameQuery } from 'src/app/games/+state';
-import { PlayerQuery, Player } from '../../player/+state';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { UnitService } from "../../unit/+state";
+import { GameService, GameQuery } from "src/app/games/+state";
+import { PlayerQuery, Player, PlayerService } from "../../player/+state";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-bottom-bar',
-  templateUrl: './bottom-bar.component.html',
-  styleUrls: ['./bottom-bar.component.scss']
+  selector: "app-bottom-bar",
+  templateUrl: "./bottom-bar.component.html",
+  styleUrls: ["./bottom-bar.component.scss"],
 })
 export class BottomBarComponent implements OnInit {
-  public gameStatus$: Observable<'waiting' | 'unit creation' | 'placement' | 'battle' | 'finished'>;
+  public gameStatus$: Observable<
+    "waiting" | "unit creation" | "placement" | "battle" | "finished"
+  >;
   public players$: Observable<Player[]>;
   public isPlayerReady$: Observable<boolean>;
 
@@ -23,18 +25,20 @@ export class BottomBarComponent implements OnInit {
     private gameQuery: GameQuery,
     private gameService: GameService,
     private playerQuery: PlayerQuery,
+    private playerService: PlayerService,
   ) {
     this.gameStatus$ = this.gameQuery.gameStatus$;
-    this.players$  = this.playerQuery.selectAll();
+    this.players$ = this.playerQuery.selectAll();
     this.isPlayerReady$ = this.gameQuery.isPlayerReady;
     this.matIconRegistry.addSvgIcon(
-      'fight',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/img/fight.svg')
+      "fight",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        "../../../assets/img/fight.svg"
+      )
     );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setReady() {
     const playerId = this.playerQuery.getActiveId();
@@ -44,10 +48,12 @@ export class BottomBarComponent implements OnInit {
     this.gameService.markReady(playerId);
 
     // Save the units created
-    if (gameStatus === 'unit creation') {
+    if (gameStatus === "unit creation") {
       this.unitService.setUnits();
     }
-
   }
 
+  skipTurn() {
+    this.playerService.switchActivePlayer();
+  }
 }
