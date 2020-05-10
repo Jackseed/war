b910f66eda5ef8747e02311f6e28365c961df306
Jackseed <vinkho.c@gmail.com>
2020-05-10 09:30:10 +0200

    action buttons

--- src/app/board/player/player.module.ts
+++ src/app/board/player/player.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PlayerBoardComponent } from "./player-board/player-board.component";
 import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+
 @NgModule({
   declarations: [PlayerBoardComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   exports: [PlayerBoardComponent],
 })
 export class PlayerModule {}
