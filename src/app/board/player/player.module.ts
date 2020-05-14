d7a3651ef1758cd8c75f07e3f31a42cef1f7f574
Jackseed <vinkho.c@gmail.com>
2020-05-14 06:33:36 +0200

    unit board embed in player board

--- src/app/board/player/player.module.ts
+++ src/app/board/player/player.module.ts
@@ -4,10 +4,17 @@ import { PlayerBoardComponent } from "./player-board/player-board.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { UnitsModule } from "../unit/units.module";
 
 @NgModule({
   declarations: [PlayerBoardComponent],
-  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    UnitsModule,
+  ],
   exports: [PlayerBoardComponent],
 })
 export class PlayerModule {}
