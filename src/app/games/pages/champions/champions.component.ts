4d31be4c27ced29d4d5fa922f20efa6088b54218
Jackseed <vinkho.c@gmail.com>
2020-05-25 18:39:53 +0200

    sort by name

--- src/app/games/pages/champions/champions.component.ts
+++ src/app/games/pages/champions/champions.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit, ViewChild, OnDestroy } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
 import { User, AuthQuery } from "src/app/auth/+state";
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
   selector: "app-champions",
   templateUrl: "./champions.component.html",
   styleUrls: ["./champions.component.scss"],
 })
-export class ChampionsComponent implements OnInit {
+export class ChampionsComponent implements OnInit, OnDestroy {
   users$: Observable<User[]>;
   user: User;
+  dataSource = new MatTableDataSource<User>();
+  dataSubscription: Subscription;
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(
     private authQuery: AuthQuery,
@@ -29,5 +35,13 @@ export class ChampionsComponent implements OnInit {
   ngOnInit(): void {
     this.users$ = this.authQuery.selectAll();
     this.user = this.authQuery.getActive();
+    this.dataSubscription = this.users$.subscribe((users) => {
+      this.dataSource.data = users;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dataSubscription.unsubscribe();
   }
 }
