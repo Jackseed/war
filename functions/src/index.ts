0545c648400dfc73317e623c7f50146cd36f0952
Jackseed <vinkho.c@gmail.com>
2020-09-21 23:00:12 +0200

    function working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,9 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-// Since this code will be running in the Cloud Functions environment
-// we call initialize Firestore without any arguments because it
-// detects authentication from the environment.
 const db = admin.firestore();
 
 // Create a new function which is triggered on changes to /status/{uid}
-// Note: This is a Realtime Database trigger, *not* Cloud Firestore.
 exports.onUserStatusChanged = functions.database
   .ref("/status/{uid}")
   .onUpdate(async (change, context) => {
@@ -18,11 +14,13 @@ exports.onUserStatusChanged = functions.database
     if (eventStatus.status === "offline") {
       const userGamesToDeleteDocs: any[] = [];
 
+      // TODO: improve query
       //  Get instant games where user is alone
       db.collection("games")
         .get()
         .then(snapshot => {
           snapshot.forEach(doc => {
+            console.log("All games scan: ", doc.data().id);
             if (
               doc.data().playerIds.includes(context.params.uid) &&
               doc.data().playerIds.length === 1 &&
@@ -35,30 +33,29 @@ exports.onUserStatusChanged = functions.database
         .catch(err => {
           console.log("Error getting documents", err);
         });
+      functions.logger.log("Games to delete:", userGamesToDeleteDocs);
 
-      console.log(userGamesToDeleteDocs);
       // It is likely that the Realtime Database change that triggered
       // this event has already been overwritten by a fast change in
       // online / offline status, so we'll re-read the current data
       // and compare the timestamps.
       const statusSnapshot = await change.after.ref.once("value");
       const status = statusSnapshot.val();
-      console.log(status, eventStatus);
+
       // If the current timestamp for this data is newer than
       // the data that triggered this event, we exit this function.
       if (status.last_changed > eventStatus.last_changed) {
         return null;
       }
 
+      const batch = db.batch();
       // delete all games
-      let batch = db.batch();
-
       userGamesToDeleteDocs.forEach(gameDoc => {
+        functions.logger.log("Deleting game doc", gameDoc);
+        functions.logger.log("Deleting subcollection player doc", gameDoc.ref.collection("players").doc(context.params.uid));
         batch.delete(gameDoc.ref);
         // delete also subcollections
-        gameDoc.docs.forEach((doc: admin.firestore.DocumentSnapshot) => {
-          batch.delete(doc.ref);
-        });
+        batch.delete(gameDoc.ref.collection("players").doc(context.params.uid));
       });
 
       return batch.commit();
