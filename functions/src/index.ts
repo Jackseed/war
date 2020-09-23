a880fb49ba2a0d066feb442d9406474c36ac7514
Jackseed <vinkho.c@gmail.com>
2020-09-24 00:56:39 +0200

    clean function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,58 +8,36 @@ const db = admin.firestore();
 exports.onUserStatusChanged = functions.database
   .ref("/status/{uid}")
   .onUpdate(async (change, context) => {
-    // Get the data written to Realtime Database
-    const eventStatus = change.after.val();
+    try {
+      const eventStatus = change.after.val();
 
-    if (eventStatus.status === "offline") {
-      const userGamesToDeleteDocs: any[] = [];
-
-      // TODO: improve query
-      //  Get instant games where user is alone
-      db.collection("games")
-        .get()
-        .then(snapshot => {
-          snapshot.forEach(doc => {
-            console.log("All games scan: ", doc.data().id);
-            if (
-              doc.data().playerIds.includes(context.params.uid) &&
-              doc.data().playerIds.length === 1 &&
-              doc.data().isInstant
-            ) {
-              userGamesToDeleteDocs.push(doc);
-            }
-          });
-        })
-        .catch(err => {
-          console.log("Error getting documents", err);
+      if (eventStatus.status === "offline") {
+        const batch = db.batch();
+        // look for user instant games
+        const snapshot = await db
+          .collection("games")
+          .where("playerIds", "array-contains", context.params.uid)
+          .where("isInstant", "==", true)
+          .get();
+        // select the games where the player is alone
+        snapshot.forEach(doc => {
+          if (doc.data().playerIds.length === 1) {
+            // delete games & subcollections
+            batch.delete(doc.ref);
+            batch.delete(doc.ref.collection("players").doc(context.params.uid));
+            return null;
+          } else {
+            return null;
+          }
         });
-      functions.logger.log("Games to delete:", userGamesToDeleteDocs);
 
-      // It is likely that the Realtime Database change that triggered
-      // this event has already been overwritten by a fast change in
-      // online / offline status, so we'll re-read the current data
-      // and compare the timestamps.
-      const statusSnapshot = await change.after.ref.once("value");
-      const status = statusSnapshot.val();
-
-      // If the current timestamp for this data is newer than
-      // the data that triggered this event, we exit this function.
-      if (status.last_changed > eventStatus.last_changed) {
-        return null;
+        return batch
+          .commit()
+          .then(_ => console.log("all good"))
+          .catch(error => console.log(error));
       }
-
-      const batch = db.batch();
-      // delete all games
-      userGamesToDeleteDocs.forEach(gameDoc => {
-        functions.logger.log("Deleting game doc", gameDoc);
-        functions.logger.log("Deleting subcollection player doc", gameDoc.ref.collection("players").doc(context.params.uid));
-        batch.delete(gameDoc.ref);
-        // delete also subcollections
-        batch.delete(gameDoc.ref.collection("players").doc(context.params.uid));
-      });
-
-      return batch.commit();
-    } else {
       return null;
+    } catch (error) {
+      console.log("Error ", error);
     }
   });
