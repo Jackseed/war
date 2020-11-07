82cdde7606fa145e95e948008846e8680997515d
Jackseed <vinkho.c@gmail.com>
2020-11-07 16:39:52 +0100

    ngSwitch template

--- src/app/games/pages/tuto/tuto.component.ts
+++ src/app/games/pages/tuto/tuto.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-tuto',
-  templateUrl: './tuto.component.html',
-  styleUrls: ['./tuto.component.scss']
+  selector: "app-tuto",
+  templateUrl: "./tuto.component.html",
+  styleUrls: ["./tuto.component.scss"]
 })
 export class TutoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  step: string;
+  constructor() {
+    this.step = "welcome";
   }
 
+  ngOnInit(): void {}
+
+  public switchStep(nextStep: string) {
+    this.step = nextStep;
+  }
 }
